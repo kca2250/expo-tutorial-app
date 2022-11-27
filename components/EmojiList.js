a1617c8def414e1bce7e6000422d4dfb0b3c66e2
kca2250 <2250katok@gmail.com>
2022-11-27 11:41:21 +0900

    絵文字リストコンポーネント作成

---
+++ components/EmojiList.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { StyleSheet, FlatList, Image, Platform, Pressable } from "react-native";
+
+export default function EmojiList({ onSelect, onCloseModal }) {
+  const [emoji] = useState([
+    require("../assets/emoji/onigiri_gomoku.png"),
+    require("../assets/emoji/onigiri_hijiki.png"),
+    require("../assets/emoji/onigiri_nori.png"),
+    require("../assets/emoji/onigiri_sekihan.png"),
+    require("../assets/emoji/onigiri_takana .png"),
+    require("../assets/emoji/onigiri_wakame.png"),
+  ]);
+
+  return (
+    <FlatList
+      horizontal
+      showsHorizontalScrollIndicator={Platform.OS === "web" ? true : false}
+      data={emoji}
+      contentContainerStyle={styles.listContainer}
+      renderItem={({ item, index }) => {
+        return (
+          <Pressable
+            onPress={() => {
+              onSelect(item);
+              onCloseModal();
+            }}
+          >
+            <Image source={item} key={index} style={styles.image} />
+          </Pressable>
+        );
+      }}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  listContainer: {
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginRight: 20,
+  },
+});
